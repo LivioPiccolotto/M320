c974db005cb78f7c7a422076bc1cea826aac2f62
LivioPiccolotto <111734364+LivioPiccolotto@users.noreply.github.com>
2022-12-04 21:09:37 +0100

    Add files via upload

---
+++ Geo_Figuren/Geo_Figuren/Figuren.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geo_Figuren
+{
+    internal class Figuren
+    {
+
+
+        class kreis
+        {
+            public string Name { get; set; }
+            
+        }
+
+        class rechteck
+        {
+            public string Name { get; set; }
+        }
+
+
+        class dreick
+        {
+
+             public string Name { get; set; }
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
